baa77dfc1044c70f38f78e4e023cae161c35c13c
Manuele Menozzi <manuele.menozzi@gmail.com>
2017-02-18 11:57:42 +0100

    Allow to configure xdebug cookie value and cgi bin path

--- src/Webgriffe_FunctionalTest_Trait.php
+++ src/Webgriffe_FunctionalTest_Trait.php
@@ -12,12 +12,21 @@ trait Webgriffe_FunctionalTest_Trait
      */
     protected static function createClient($setXdebugCookie = false)
     {
+        $phpCgiBin = (string)Mage::getConfig()->getNode('phpunit/functional/php_cgi_bin') ?: 'php-cgi';
         $client = new Client(
-            new CgiHttpKernel(Mage::getBaseDir(), 'index.php'),
+            new CgiHttpKernel(Mage::getBaseDir(), 'index.php', $phpCgiBin),
             ['HTTP_HOST' => parse_url(Mage::getBaseUrl(), PHP_URL_HOST)]
         );
         if ($setXdebugCookie) {
-            $client->getCookieJar()->set(new Cookie('XDEBUG_SESSION', 'PHPSTORM'));
+            $xdebugSession = (string)Mage::getConfig()->getNode('phpunit/functional/xdebug_session');
+            if (!$xdebugSession) {
+                throw new \RuntimeException(
+                    'Cannot set xDebug cookie, its value is not configured. Please, define an "XDEBUG_SESSION" ' .
+                    'cookie value, according to your IDE settings, at config path "phpunit/functional/xdebug_session" '.
+                    'in your app/etc/local.xml.'
+                );
+            }
+            $client->getCookieJar()->set(new Cookie('XDEBUG_SESSION', $xdebugSession));
         }
         return $client;
     }
@@ -28,13 +37,13 @@ trait Webgriffe_FunctionalTest_Trait
      */
     protected static function openResponseInBrowser(Client $client)
     {
-        $openCommand = (string)Mage::getConfig()->getNode('phpunit/browser/open_command');
+        $openCommand = (string)Mage::getConfig()->getNode('phpunit/functional/open_browser_command');
         if (!$openCommand) {
             throw new \RuntimeException(
                 'Cannot open response in browser. Open command is not configured. Please define a browser open ' .
-                'command at config path "phpunit/browser/open_command" in your app/etc/local.xml file. Command ' .
-                'must be like "open \'%s\'" (where "%s" will be replaced with the file which contains the response ' .
-                'to open in browser).'
+                'command at config path "phpunit/functional/open_browser_command" in your app/etc/local.xml file. ' .
+                'Command must be like "open \'%s\'" (where "%s" will be replaced with the file which contains the ' .
+                'response to open in browser).'
             );
         }
         $tmpFile = tempnam(sys_get_temp_dir(), 'response_') . '.html';
